32a2020ef0a4bc48f1139519367ecc65daf61274
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-06-18 06:50:04 +0700

    refactor: Wrap draw grid code to a function

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const grid = document.querySelector('#grid');
+    const setGridBtn = document.querySelector('setGridSize');
+
     const size = 16;
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            square.addEventListener('mouseenter', function() {
-                square.classList.add('hover');
-            });
-            grid.appendChild(square);   
+    let currentSize = size;
+
+    function createGrid(size) {
+        grid.innerHTML = '';    // clear existing grid if any
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                const square = document.createElement('div');
+                square.classList.add('square');
+                square.addEventListener('mouseenter', function() {
+                    square.classList.add('hover');
+                });
+                grid.appendChild(square);   
+            }
         }
     }
+    
+    
+    document.documentElement.style.setProperty('--grid-size', currentSize);
+    createGrid(currentSize);
 });
